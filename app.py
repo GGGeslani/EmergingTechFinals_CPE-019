5ec4941d76e986317507ef38366b0e58bc25b012
GGGeslani <111827267+GGGeslani@users.noreply.github.com>
2023-05-26 23:14:32 +0800

    app.py

--- app.py
+++ app.py
@@ -2,38 +2,27 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
+import cv2
 
 def main():
-    # Set up the Streamlit app
-    st.markdown(
-        """
-        <style>
-        body {
-            background-image: url('BG2.jpg');
-            background-size: cover;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
+    # set up the Streamlit app
     st.write("Members:")
     st.write("Kenneth Cordero")
     st.write("Grant Guriel Geslani")
     st.write("Aaron Jan Inalyo")
-    st.write("Course & Section: CPE019 - CPE32S3")
+    st.write("Course &n Section: CPE019 - CPE32S3")
     st.write("Instructor: Engr. Roman Richard")
     st.title("Gastropod Mollusk Classifier (Slug/Snail)")
-    st.write("This app classifies whether an uploaded image contains a Slug or Snail using a pre-trained convolutional neural network model.")
-
-    @st.cache(allow_output_mutation=True)
+    st.write("This app classifies whether an uploaded image contains a Slug or Snail images using a pre-trained convolutional neural network model.")
+   
+    @st.cache_resource
     def load_model():
         model = tf.keras.models.load_model('weights-improvement-46-0.91.hdf5')
         return model
-
+    
     def import_and_predict(image_data, model):
-        size = (128, 128)
-        image = ImageOps.fit(image_data, size, Image.LANCZOS)
+        size=(128,128)
+        image = ImageOps.fit(image_data,size, Image.LANCZOS)
         image = np.asarray(image)
         image = image / 255.0
         img_reshape = np.reshape(image, (1, 128, 128, 3))
@@ -42,6 +31,7 @@ def main():
 
     model = load_model()
     class_names = ["SLUG", "SNAIL"]
+    
 
     file = st.file_uploader("Choose a Gastropod picture from your computer", type=["jpg", "png", "jpeg"])
 
@@ -55,6 +45,6 @@ def main():
         class_name = class_names[class_index]
         string = "Prediction: " + class_name
         st.success(string)
-
+ 
 if __name__ == "__main__":
     main()
