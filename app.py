0ac563535bcf03f1e8f39fc732255da71f5795c6
GGGeslani <111827267+GGGeslani@users.noreply.github.com>
2023-05-26 23:13:11 +0800

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
-import cv2
 
 def main():
     # Set up the Streamlit app
@@ -22,19 +21,19 @@ def main():
     st.write("Kenneth Cordero")
     st.write("Grant Guriel Geslani")
     st.write("Aaron Jan Inalyo")
-    st.write("Course &n Section: CPE019 - CPE32S3")
+    st.write("Course & Section: CPE019 - CPE32S3")
     st.write("Instructor: Engr. Roman Richard")
     st.title("Gastropod Mollusk Classifier (Slug/Snail)")
-    st.write("This app classifies whether an uploaded image contains a Slug or Snail images using a pre-trained convolutional neural network model."
-   
-    @st.cache_resource
+    st.write("This app classifies whether an uploaded image contains a Slug or Snail using a pre-trained convolutional neural network model.")
+
+    @st.cache(allow_output_mutation=True)
     def load_model():
         model = tf.keras.models.load_model('weights-improvement-46-0.91.hdf5')
         return model
-    
+
     def import_and_predict(image_data, model):
-        size=(128,128)
-        image = ImageOps.fit(image_data,size, Image.LANCZOS)
+        size = (128, 128)
+        image = ImageOps.fit(image_data, size, Image.LANCZOS)
         image = np.asarray(image)
         image = image / 255.0
         img_reshape = np.reshape(image, (1, 128, 128, 3))
@@ -43,7 +42,6 @@ def main():
 
     model = load_model()
     class_names = ["SLUG", "SNAIL"]
-    
 
     file = st.file_uploader("Choose a Gastropod picture from your computer", type=["jpg", "png", "jpeg"])
 
@@ -57,6 +55,6 @@ def main():
         class_name = class_names[class_index]
         string = "Prediction: " + class_name
         st.success(string)
- 
+
 if __name__ == "__main__":
     main()
